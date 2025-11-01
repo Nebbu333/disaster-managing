fa9535c062146edc7fb6492537db3106b438e5c7
Nebbu333 <nebilasalh@gmail.com>
2025-11-01 20:04:00 +0300

    footer and other page updated

--- src/App.js
+++ src/App.js
@@ -1,80 +1,33 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Testimonials from "./pages/Testimonials";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 import "./App.css";
 
-// Navbar Component
-const NavBar = () => {
-  return (
-    <nav className="navbar">
-      <div className="nav-left">
-        <Link to="/" className="nav-link">
-          Home
-        </Link>
-        <Link to="/contact" className="nav-link">
-          Contact Us
-        </Link>
-        <Link to="/testimonials" className="nav-link">
-          Testimonials
-        </Link>
-        <Link to="/about" className="nav-link">
-          About Us
-        </Link>
-      </div>
-      <div className="nav-right">
-        <Link to="/login" className="login-btn">
-          Login
-        </Link>
-        <Link to="/signup" className="signup-btn">
-          Sign Up
-        </Link>
-      </div>
-    </nav>
-  );
-};
-
-// Footer Component
-const Footer = () => {
-  return (
-    <footer className="footer">
-      <div className="footer-links">
-        <Link to="/" className="footer-link">
-          Home
-        </Link>
-        <Link to="/contact" className="footer-link">
-          Contact Us
-        </Link>
-        <Link to="/testimonials" className="footer-link">
-          Testimonials
-        </Link>
-        <Link to="/about" className="footer-link">
-          About Us
-        </Link>
-      </div>
-      <p>&copy; 2025 Disaster Management Website</p>
-    </footer>
-  );
-};
-
-// App Component
 function App() {
   return (
     <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/testimonials" element={<Testimonials />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-      <Footer />
+      <div className="app-container">
+        <NavBar />
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/testimonials" element={<Testimonials />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
     </Router>
   );
 }
